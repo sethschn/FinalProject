372db0d00a08dddcca713d46d8fa6551d472a7d8
Rowland1318 <christina.a.rowland@gmail.com>
2020-04-28 12:05:45 -0600

    completed the resource entity and junit test

---
+++ JPAOutDistancing/src/main/java/com/skilldistillery/outdistancing/entities/Resource.java
@@ -0,0 +1,105 @@
+package com.skilldistillery.outdistancing.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Resource {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String description;
+	private String link;
+	
+	@Column (name = "image_url")
+	private String imageUrl;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public Resource(int id, String name, String description, String link, String imageUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.link = link;
+		this.imageUrl = imageUrl;
+	}
+
+	public Resource() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Resource other = (Resource) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Resource [id=" + id + ", name=" + name + ", description=" + description + ", link=" + link
+				+ ", imageUrl=" + imageUrl + "]";
+	}
+	
+	
+
+}
