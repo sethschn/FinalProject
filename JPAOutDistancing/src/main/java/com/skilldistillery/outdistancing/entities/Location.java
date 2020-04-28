1564611491e4242dc6a604817165c3ad67d7e71e
Tim Teruel <timteruel@Administrators-MacBook-Pro.local>
2020-04-28 15:14:42 -0600

    location entity created and tests pass

---
+++ JPAOutDistancing/src/main/java/com/skilldistillery/outdistancing/entities/Location.java
@@ -0,0 +1,191 @@
+package com.skilldistillery.outdistancing.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+	
+	//F I E L D S 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int id;
+	
+	public String street;
+	
+	public String city;
+	
+	public String state;
+	
+	public String country;
+	
+	@Column(name = "postal_code")
+	public String postalCode;
+	
+	public String title;
+	
+	@Column(name = "location_url")
+	public String locationUrl;
+	
+	//CONSTRUCTORS
+	public Location() {}
+
+	public Location(int id, String street, String city, String state, String country, String postalCode, String title,
+			String locationUrl) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.country = country;
+		this.postalCode = postalCode;
+		this.title = title;
+		this.locationUrl = locationUrl;
+	}
+
+	//GETTERS & SETTERS
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLocationUrl() {
+		return locationUrl;
+	}
+
+	public void setLocationUrl(String locationUrl) {
+		this.locationUrl = locationUrl;
+	}
+
+	//TOSTRING
+	@Override
+	public String toString() {
+		return "Location [id=" + id + ", street=" + street + ", city=" + city + ", state=" + state + ", country="
+				+ country + ", postalCode=" + postalCode + ", title=" + title + ", locationUrl=" + locationUrl + "]";
+	}
+
+	//HASHCODE & EQUALS
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((locationUrl == null) ? 0 : locationUrl.hashCode());
+		result = prime * result + ((postalCode == null) ? 0 : postalCode.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (id != other.id)
+			return false;
+		if (locationUrl == null) {
+			if (other.locationUrl != null)
+				return false;
+		} else if (!locationUrl.equals(other.locationUrl))
+			return false;
+		if (postalCode == null) {
+			if (other.postalCode != null)
+				return false;
+		} else if (!postalCode.equals(other.postalCode))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+	
+}
