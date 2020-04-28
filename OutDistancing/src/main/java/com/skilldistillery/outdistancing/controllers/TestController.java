5140e35b63bf730a51836c0ca3c20c6175c5365d
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-04-28 11:35:23 -0600

    Stubbed controller with the CrossOrigin annotation with the localhost port

--- OutDistancing/src/main/java/com/skilldistillery/outdistancing/controllers/TestController.java
+++ OutDistancing/src/main/java/com/skilldistillery/outdistancing/controllers/TestController.java
@@ -1,9 +1,11 @@
 package com.skilldistillery.outdistancing.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin({ "*", "http://localhost:4220" })
 public class TestController {
 	
 	@GetMapping("ping")
