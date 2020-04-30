bf3bff501bf41d13bf7bca744b3595dd7a7c5b72
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-04-30 09:25:51 -0600

    Stubbed out Event, EventComment service and models

---
+++ ngOutDistancing/src/app/models/eventcomment.spec.ts
@@ -0,0 +1,7 @@
+import { Eventcomment } from './eventcomment';
+
+describe('Eventcomment', () => {
+  it('should create an instance', () => {
+    expect(new Eventcomment()).toBeTruthy();
+  });
+});
