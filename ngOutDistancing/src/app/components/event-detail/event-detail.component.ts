67373fea9d5e917937d614e459c4ac0136116a76
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-04-30 10:40:41 -0600

    Put in components, modules and services in the app.module.ts

--- ngOutDistancing/src/app/components/event-detail/event-detail.component.ts
+++ ngOutDistancing/src/app/components/event-detail/event-detail.component.ts
@@ -1,4 +1,8 @@
+import { Event } from './../../models/event';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EventService } from 'src/app/services/event.service';
+
 
 @Component({
   selector: 'app-event-detail',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventDetailComponent implements OnInit {
 
-  constructor() { }
+
+  selected = null;
+  newEvent = new Event();
+
+  events: Event[] = [];
+
+  constructor(private eventSvc: EventService, private currentRoute: ActivatedRoute, private router: Router) { }
+
 
   ngOnInit(): void {
+    if (!this.selected && this.currentRoute.snapshot.paramMap.get('id')) {
+      this.eventSvc.show(this.currentRoute.snapshot.paramMap.get('id')).subscribe(
+        data => {
+          this.selected = data;
+        },
+        bad => {
+          this.router.navigateByUrl('home')
+          console.error('EventDetailComponent.reload(): error retrieving event detail list');
+          console.error(bad);
+        }
+        );
+      }
+      this.loadEvents();
+    }
+
+
+  loadEvents(){
+    this.eventSvc.index().subscribe(
+      data => {this.events = data;
+      },
+      bad => {
+        console.error("GratitudeListComponent.loadEntries(): error loading");
+        console.error(bad);
+      }
+    );
   }
 
 }
