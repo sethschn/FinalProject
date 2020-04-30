1bcc26ae5c3bb69033bfab1d9fe7a07805902d3f
Rowland1318 <christina.a.rowland@gmail.com>
2020-04-30 09:53:25 -0600

    resource model and service complete, needs to be tested

---
+++ ngOutDistancing/src/app/models/resource.ts
@@ -0,0 +1,16 @@
+export class Resource {
+  id: number;
+  name: string;
+  description: string;
+  link: string;
+  imageUrl: string;
+
+
+constructor(id?: number, name?: string, description?: string, link?: string, imageUrl?: string) {
+  this.id = id;
+  this.name = name;
+  this.description = description;
+  this.link = link;
+  this.imageUrl = imageUrl;
+}
+}
