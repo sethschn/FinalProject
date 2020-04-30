dccb68f9428c5028c0ed1c397d5605f2575f8a42
Rowland1318 <christina.a.rowland@gmail.com>
2020-04-29 19:00:23 -0600

    fixed the user authentication on the user class

--- OutDistancing/src/main/java/com/skilldistillery/outdistancing/services/UserService.java
+++ OutDistancing/src/main/java/com/skilldistillery/outdistancing/services/UserService.java
@@ -6,9 +6,9 @@ import com.skilldistillery.outdistancing.entities.User;
 
 public interface UserService {
 	List<User> listAllUsers();
-	User findById(int userId);
+	User findById(int userId, String username);
 //	User createUser(User user);
-	User updateUser (int userId, User user);
-	Boolean changeUserEnabled(String username);
+	User updateUser (int userId, User user, String username);
+	Boolean changeUserEnabled(int UserId, String username);
 	User findUserByUsername(String username);
 }
