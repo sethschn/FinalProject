1d6054f236acd9cacae62fd41497a3f0aa7f30a9
Rowland1318 <christina.a.rowland@gmail.com>
2020-04-30 14:00:03 -0600

    trying to test admin page

--- ngOutDistancing/src/app/components/admin/admin.component.ts
+++ ngOutDistancing/src/app/components/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Resource } from 'src/app/models/resource';
+import { ResourceService } from 'src/app/services/resource.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  selected = null;
+  newResource = new Resource();
+  editResource = null;
+
+  resourceList: Resource[] = [];
+
+  constructor(private resourceSvc: ResourceService, private currentRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    if (!this.selected && this.currentRoute.snapshot.paramMap.get('id')) {
+      this.resourceSvc.showResource(this.currentRoute.snapshot.paramMap.get('id')).subscribe(
+        data => {
+          this.selected = data;
+        },
+        bad => {
+          this.router.navigateByUrl('eventDetail')
+          console.error('EventDetailComponent.reload(): error retrieving event detail list');
+          console.error(bad);
+        }
+        );
+      }
+      this.loadResources();
   }
+
+  displayResource(resource){
+    this.router.navigateByUrl(`/resources/${resource.id}`);
+ }
+
+ showResource(id){
+  this.resourceSvc.showResource((this.currentRoute.snapshot.paramMap.get('id'))).subscribe(
+    good => {
+      this.loadResources();
+    },
+    bad => {
+      console.error("Admin.show(): error")
+    }
+  )
+};
+
+displayTable(){
+  this.selected = null;
+}
+
+loadResources(){
+  this.resourceSvc.indexResource().subscribe(
+    data => {this.resourceList = data;
+    },
+    bad => {
+      console.error("EventDetailComponent.loadEvents(): error loading");
+      console.error(bad);
+    }
+  );
+}
+
+createResource(resource: Resource){
+  console.log(event);
+  this.resourceSvc.createResource(resource).subscribe(
+    good => {
+      this.loadResources();
+      this.newResource = new Resource();
+    },
+    bad => {
+      console.error("AdminComponent.createResource(): error adding");
+      console.error(bad);
+    })
+}
+
+setEditResource(){
+  this.editResource = Object.assign({}, this.selected)
+}
+
+updateResource(resource: Resource){
+  console.log(event);
+ this.resourceSvc.updateResource(resource).subscribe(
+   good =>{
+     this.loadResources();
+     this.editResource= null;
+     this.selected = this.editResource;
+   },
+   bad => {
+     console.error("AdminComponent.updateResource(): error updating");
+     console.error(bad);
+   })
+}
+
+deleteResource(resourceId){
+  this.resourceSvc.deleteResource(resourceId).subscribe(
+
+    data => {
+      this.loadResources();
+      this.selected = null;
+
+    },
+    err => {
+      console.error("EventDetailListComponent.deleteEvent(): error deleting" + err);
+    })
+  };
 
 }
