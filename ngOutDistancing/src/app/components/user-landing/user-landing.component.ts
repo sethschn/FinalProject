ab9123704431675074e7823057147eca3a5682f4
Rowland1318 <christina.a.rowland@gmail.com>
2020-05-01 13:35:02 -0600

    fixed the cards for activities and events

--- ngOutDistancing/src/app/components/user-landing/user-landing.component.ts
+++ ngOutDistancing/src/app/components/user-landing/user-landing.component.ts
@@ -1,10 +1,11 @@
-import { ActivityComponent } from './../activity/activity.component';
+import { EventService } from './../../services/event.service';
 import { AuthService } from './../../services/auth.service';
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ActivityService } from './../../services/activity.service';
 import { Activity } from 'src/app/models/activity';
+import { Event } from 'src/app/models/event';
 
 
 @Component({
@@ -17,18 +18,32 @@ export class UserLandingComponent implements OnInit {
   activity = new Activity();
   activities: Activity[] = [];
 
+  event = new Event();
+  events: Event[] = [];
+
   constructor(private userService: UserService,private currentRoute: ActivatedRoute, private router: Router, private authService: AuthService,
-    private activitySvc: ActivityService
+    private activitySvc: ActivityService, private eventSvc: EventService
     ) { }
 
   ngOnInit(): void {
     this.loadActivities();
+    this.loadEvents();
   }
 
   loadActivities(){
     this.activitySvc.index().subscribe(
       data => {
         this.activities = data;
+      },
+      err => {
+        console.error('Error in our loadActivities() method. ' + err);
+      }
+    );
+  }
+  loadEvents(){
+    this.eventSvc.index().subscribe(
+      data => {
+        this.events = data;
       },
       err => {
         console.error('Error in our loadActivities() method. ' + err);
