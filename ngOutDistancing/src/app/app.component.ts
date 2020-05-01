9f43db8ffde51d020ded8f7fccd4ebd270e77f1a
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-04-30 19:21:24 -0600

    Added markers on map

--- ngOutDistancing/src/app/app.component.ts
+++ ngOutDistancing/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements AfterViewInit{
 
   mapOptions: google.maps.MapOptions = {
     center: this.coordinates,
-    zoom: 8,
+    zoom: 10,
   };
 
   marker = new google.maps.Marker({
@@ -26,13 +26,62 @@ export class AppComponent implements AfterViewInit{
     map: this.map,
   });
 
+  markers = [
+    {
+      position: new google.maps.LatLng(39.6088, -104.9027),
+      map: this.map,
+      title: "Skill Distillery"
+    },
+    {
+      position: new google.maps.LatLng(39.5883, -105.6438),
+      map: this.map,
+      title: "Mount Evans"
+    }
+  ];
+  constructor(){
+
+  }
+
   ngAfterViewInit(): void {
     this.mapInitializer();
   }
+  loadAllMarkers(): void {
+    this.markers.forEach(markerInfo => {
+      //Creating a new marker object
+      const marker = new google.maps.Marker({
+        ...markerInfo
+      });
 
-  mapInitializer() {
-    this.map = new google.maps.Map(this.gmap.nativeElement,
-    this.mapOptions);
+      //creating a new info window with markers info
+      const infoWindow = new google.maps.InfoWindow({
+        content: marker.getTitle()
+      });
+
+      //Add click event to open info window on marker
+      marker.addListener("click", () => {
+        infoWindow.open(marker.getMap(), marker);
+      });
+
+      //Adding marker to google map
+      marker.setMap(this.map);
+    });
+  }
+
+  mapInitializer(): void {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+
+    //Adding Click event to default marker
+    this.marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: this.marker.getTitle()
+      });
+      infoWindow.open(this.marker.getMap(), this.marker);
+    });
+
+    //Adding default marker to map
     this.marker.setMap(this.map);
-   }
+
+    //Adding other markers
+    this.loadAllMarkers();
+  }
 }
