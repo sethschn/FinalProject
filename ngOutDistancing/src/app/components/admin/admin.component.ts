83a24f0149d7c1da1d9901e756d509a8f4a8e741
Rowland1318 <christina.a.rowland@gmail.com>
2020-05-03 15:04:36 -0600

    moving all the lists into admin

--- ngOutDistancing/src/app/components/admin/admin.component.ts
+++ ngOutDistancing/src/app/components/admin/admin.component.ts
@@ -1,4 +1,11 @@
+import { ActivityComponent } from './../activity/activity.component';
 import { Component, OnInit } from '@angular/core';
+import { Activity } from 'src/app/models/activity';
+import { ActivityComment } from 'src/app/models/activity-comment';
+import { ActivityService } from 'src/app/services/activity.service';
+import { ActivityCommentService } from 'src/app/services/activity-comment.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-admin',
@@ -7,10 +14,173 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  activity = new Activity();
+  activities: Activity[] = [];
+  activityComments: ActivityComment[] = [];
+  closeResult = '';
+  comment = new ActivityComment();
+  currentActivityId = null;
+  editCurrentActivity = null;
+  newActivity = new Activity();
+  newComment = new ActivityComment();
+  selectedActivity = null;
+
+  constructor(
+
+    private activitySvc: ActivityService,
+    private commentSvc: ActivityCommentService,
+    private currentRoute: ActivatedRoute,
+    private router: Router,
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
+    const activityIdStr = this.currentRoute.snapshot.paramMap.get('id');
+    console.log('selected:' + this.selectedActivity);
+    console.log('activityIdStr: ' + activityIdStr);
+    if (!this.selectedActivity && activityIdStr) {
+      this.currentActivityId = activityIdStr;
+      const activityId = Number.parseInt(activityIdStr,10);
+      this.activitySvc.show(activityId).subscribe(
+        activity =>{
+          this.selectedActivity = activity;
+          this.loadComments();
+        },
+        fail => {
+          console.error(fail);
+          this.router.navigateByUrl(`/notFound`)
+        }
+      );
+    }
+    else{
+      this.reloadActivity();
+    }
   }
 
+  loadComments(){
+    this.commentSvc.index(this.selectedActivity.id).subscribe(
+      comments =>{
+        this.activityComments = comments;
+        console.log(comments);
+      },
+      fail => {
+        console.error(fail);
+        // this.router.navigateByUrl(`/notFound`)
+      }
+    );
+
+
+  }
+
+  reloadActivity(){
+    this.activitySvc.index().subscribe(
+      data => {
+        this.activities = data;
+      },
+      bad =>{
+        console.error('nonUserLanding.reload(): error retrieving activity list')
+        console.error(bad);
+      }
+    );
+  }
+
+  loadActivities(){
+    this.activitySvc.index().subscribe(
+      data => {
+        this.activities = data;
+      },
+      err => {
+        console.error('Error in our loadActivities() method. ' + err);
+      }
+    );
+  }
+
+  addNewActivity(activity: Activity){
+    this.activitySvc.create(activity).subscribe(
+      good => {
+        this.reloadActivity();
+        this.newActivity = new Activity();
+      },
+      bad =>{
+        console.error('workoutListComponent.addWorkout(): error adding workout')
+        console.error(bad);
+      }
+    );
+  }
+
+  updateCurrentActivity(activity: Activity){
+    console.log('update activity: ' + activity)
+    this.activitySvc.update(activity).subscribe(
+      yay => {
+        this.reloadActivity();
+        // this.editCurrentActivity = null;
+        this.selectedActivity = this.editCurrentActivity;
+      },
+      boo => {
+      console.error('activity component updating activity error');
+    }
+    );
+  }
+
+  setEditActivity(){
+    this.editCurrentActivity = Object.assign({}, this.selectedActivity);
+  }
+
+  displayActivityTable(activities){
+    this.selectedActivity = null;
+    this.router.navigateByUrl(`/activities`);
+  }
+
+  displayActivity(activity){
+    console.log('--------------------');
+    console.log(activity);
+    this.router.navigateByUrl(`/activities/${activity.id}`);
+  }
+
+  addComment(comment: ActivityComment){
+    this.commentSvc.createComment(comment, this.currentActivityId).subscribe(
+      good => {
+        this.newComment= new ActivityComment();
+        // this.selected = null;
+        console.log(this.currentActivityId);
+        // this.router.navigateByUrl(`/activities/${this.currentActivityId}`);
+        this.loadComments();
+      },
+      bad =>{
+        console.error('activity.component.ts addComment(): error adding comment')
+        console.error(bad);
+      }
+    );
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  deleteActivity(id: number) {
+    this.activitySvc.disableActivity(id).subscribe(
+      data=> {
+        this.reloadActivity();
+        this.selectedActivity = null;
+      },
+      err => {
+        console.error('error in deleting activity component')
+      }
+    )
+  }
 
 }
