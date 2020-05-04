298b83cc00cf9647e83910f77c7136a053b6df74
sethschn <sethschn301@gmail.com>
2020-05-04 10:36:47 -0600

    Added usergroup card stub

--- ngOutDistancing/src/app/components/usergroup/usergroup.component.ts
+++ ngOutDistancing/src/app/components/usergroup/usergroup.component.ts
@@ -1,8 +1,11 @@
+import { UsergroupService } from './../../services/usergroup.service';
+import { Usergroup } from './../../models/usergroup';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-usergroup',
@@ -10,13 +13,57 @@ import { Router } from '@angular/router';
   styleUrls: ['./usergroup.component.css']
 })
 export class UsergroupComponent implements OnInit {
+  group = new Usergroup();
+  groups: Usergroup[] = [];
+  creator: User = null;
+  newGroup = null;
+  selected = null;
 
   constructor( private authService: AuthService,
     private userService: UserService,
+    private groupService: UsergroupService,
     private modalService: NgbModal,
     private router: Router) { }
 
   ngOnInit(): void {
+    this.loadGroups();
+  }
+
+  loadGroups(){
+    this.groupService.index().subscribe(
+      data => {
+        console.log("----------------"+data);
+        this.groups = data;
+      },
+      err => {
+        console.error('Error in our loadGroups() method. ' + err);
+      }
+    );
+  }
+
+  displayGroup(group: Usergroup){
+    this.selected = group;
+  }
+
+  displayTable(){
+    this.selected = null;
+  }
+
+  joinGroup(group: Usergroup){
+
+  }
+
+  addNewGroup(userGroup: Usergroup){
+    this.groupService.create(userGroup).subscribe(
+      good => {
+        this.loadGroups();
+        this.newGroup = new Usergroup();
+      },
+      bad =>{
+        console.error('addNewGroupComponent.addNewGroup(): error adding group')
+        console.error(bad);
+      }
+    );
   }
 
 }
--- ngOutDistancing/src/app/models/usergroup.ts
+++ ngOutDistancing/src/app/models/usergroup.ts
@@ -4,16 +4,16 @@ export class Usergroup {
   name: string;
   shortDescription: string;
   enabled: boolean;
-  user: User;
+  creator: User;
   description: string;
   imageUrl: string;
 
-  constructor(id?: number, name?: string, shortDescription?: string, enabled?: boolean, user?: User, description?: string, imageUrl?: string){
+  constructor(id?: number, name?: string, shortDescription?: string, enabled?: boolean, creator?: User, description?: string, imageUrl?: string){
     this.id = id;
     this.name = name;
     this.shortDescription = shortDescription;
     this.enabled =  enabled;
-    this.user = user;
+    this.creator = creator;
     this.description = description;
     this.imageUrl = imageUrl;
   }
